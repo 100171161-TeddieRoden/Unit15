d4cca894141a94ee72300f7005cef643128d56cb
Teddie Roden <100171161@student.burnley.ac.uk>
2022-03-02 11:42:34 +0000

    Changed name to PHP-W3-Variables

---
+++ Testing/PHP-W3-Variables.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Page Title</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <script src='main.js'></script>
+</head>
+<body>
+<?php
+$txt = "Hello world!";
+$x = 5;
+$y = 10.5;
+$txt1 = "W3Schools.com";
+echo "I love $txt1!";
+$txt2 = "W3Schools.com";
+echo "I love " . $txt2 . "!";
+$w = 5;
+$v = 4;
+echo $x + $y;
+?>
+</body>
+</html>
